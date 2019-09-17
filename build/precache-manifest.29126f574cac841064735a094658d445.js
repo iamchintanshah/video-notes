66de111efef0d0c7e16c4f561526504f836d7070
Chintan Shah <cin8688@gmail.com>
2019-09-17 22:43:45 +0530

    Add files via upload

---
+++ build/precache-manifest.29126f574cac841064735a094658d445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce552c7ed4fca9a877b07035c1ee4e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e66c8f7527eed70111",
+    "url": "/static/css/main.5cfabec1.chunk.css"
+  },
+  {
+    "revision": "ca76d6901d24f1c6bcac",
+    "url": "/static/js/2.48ec9b0e.chunk.js"
+  },
+  {
+    "revision": "01e66c8f7527eed70111",
+    "url": "/static/js/main.066a37dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
